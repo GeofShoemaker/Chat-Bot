3ed445ffc14c17ff6ede47abc1d9fc4171af35fd
Geof Shoemaker <geof.shoemaker@students.makeschool.com>
2020-09-17 20:38:58 +0000

    Working on making chat fancier

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from random import choice
 
 def get_bot_response(user_response):
-  bot_responses1=["I love dogs too!!", "I like cats, but I think dogs are better too.", "You read my mind!"]
-  bot_responses2= ["Cats are adorable, forget about dogs.", "Great minds think alike ;)","Friendly felines forever!"]
-  print("Alrighty, bye!" if user_response=="done"else choice(bot_responses1) if user_response=="dogs" else choice(bot_responses2) if user_response=="cats"else "That wasn't an option")
-  return user_response!="done"
+  dog_resp, cat_resp =["I love dogs too!!", "I like cats, but I think dogs are better too.", "You read my mind!"],["Cats are adorable, forget about dogs.", "Great minds think alike ;)","Friendly felines forever!"]
+  print("\npet-bot > ","Alrighty, bye!" if user_response=="done" else choice(dog_resp) if user_response=="dogs" else choice(cat_resp) if user_response=="cats"else "That wasn't an option","\n")
+  return   user_response!="done"
 
-print("Welcome to the pet lovers chat bot! Here we like to talk about our pets.")
-while get_bot_response(input("Do you like dogs or cats better? ")):continue
-  
-  
+print("pet-bot > Welcome to the pet lovers chat bot! Here we like to talk about our pets.\n")
+while get_bot_response(input("Do you like dogs or cats better? Enter \"dogs\" or \"cats\"\nyou > ")):continue
+
+
+
+
+
   
